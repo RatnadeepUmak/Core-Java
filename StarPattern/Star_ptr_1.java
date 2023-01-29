ca6a4d210a7c38c72dfce7574cdc68c056eaa987
Ratnadeep Dilip Umak <ratnadeepumak@gmail.com>
2023-01-29 01:26:25 -0800

    added Star_ptr_1.java

---
+++ StarPattern/Star_ptr_1.java
@@ -0,0 +1,18 @@
+package StarPattern;
+import java.util.Scanner;
+class Star_ptr_1{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+            int n = s.nextInt();
+
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        
+
+    }
+
+}
