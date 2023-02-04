2930e6c2a4b02310eefe7b714a7a86f9a492e399
Ratnadeep Dilip Umak <ratnadeepumak@gmail.com>
2023-02-03 22:15:08 -0800

    added Taking_inp_printing_Arr.java

---
+++ ArrayUse/Taking_inp_printing_Arr.java
@@ -0,0 +1,17 @@
+package ArrayUse;
+import java.util.*;
+public class Taking_inp_printing_Arr {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int input[] = new int[size];
+
+        for(int i = 0; i < size; i++){
+            input[i] = s.nextInt();
+        }
+        for(int i = 0; i < size; i++){
+            System.out.print(input[i]+" ");
+        }
+        System.out.println();
+    }
+}
