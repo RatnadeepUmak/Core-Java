8455d7d3a58073bfed440188141bca2c26e91470
Ratnadeep Dilip Umak <ratnadeepumak@gmail.com>
2023-02-15 03:18:32 -0800

    added Basics_of_String.java

---
+++ String/Basics_of_String.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Basics_of_String {
+    public static void main(String args[]){
+        char ch[] = {'C','a','d','k'};
+        String str = "Coding";
+        System.out.println(str.length());
+        System.out.println(str.charAt(2));
+    }
+    
+}
