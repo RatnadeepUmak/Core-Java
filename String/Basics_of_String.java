d38ab9680c7dd284959527151e619638af44608f
Ratnadeep Dilip Umak <ratnadeepumak@gmail.com>
2023-02-15 03:50:18 -0800

    added Basics_of_String.java

--- String/Basics_of_String.java
+++ String/Basics_of_String.java
@@ -3,9 +3,20 @@ package String;
 public class Basics_of_String {
     public static void main(String args[]){
         char ch[] = {'C','a','d','k'};
-        String str = "Coding";
-        System.out.println(str.length());
-        System.out.println(str.charAt(2));
+        String str1 = "Coding";
+        String str2 = " is Fun";
+        String str3 = "Coding";
+
+       System.out.println(str1.contains("ding"));
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.compareTo(str3));
+        str1 = str1 + str2; // str1 += str2;
+        System.out.println(str1);
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(2));
+
+        
+        
     }
     
 }
