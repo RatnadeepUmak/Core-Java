e9e11237f68fe577ba8e8cb97579845d98833269
Ratnadeep Dilip Umak <ratnadeepumak@gmail.com>
2023-02-15 04:10:37 -0800

    added SubString.java

--- String/SubString.java
+++ String/SubString.java
@@ -1,10 +1,11 @@
 package String;
 
 public class SubString {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         String str1 = "Coding";
 
-        System.out.println(str1.substring(1,5));
+        System.out.println(str1.substring(1, 5));
         System.out.println(str1.substring(3));
+
     }
 }
